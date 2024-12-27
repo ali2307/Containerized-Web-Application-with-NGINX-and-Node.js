82ee495e9ddda3949b50999df3883f6c8d96fd14
Uthuman <159163522+ali2307@users.noreply.github.com>
2024-12-27 15:28:44 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3008u;
+const PORT = process.env.PORT || 4000u;
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 
