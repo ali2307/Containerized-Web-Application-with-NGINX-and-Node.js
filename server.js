adb6ee0126d353c2815ed830bb3dc951a1fe6ce0
Uthuman <159163522+ali2307@users.noreply.github.com>
2024-12-27 15:49:23 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 4000;
-
-console.log('jjjjjjjjjjjjjjjj');
+module.exports = {
+  apps: [
+    {
+      name: 'HealthCareApp',
+      script: 'server.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 4000
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 4000
+      }
+    }
+  ]
+};
